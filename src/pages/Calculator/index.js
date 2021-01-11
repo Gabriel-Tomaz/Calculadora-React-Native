a7e48da4c8c53b3b6ea9102b668c403ee7190cd8
Gabriel-Tomaz <jgabrieltomaz2706@gmail.com>
2021-01-11 12:08:44 -0300

    Nossa tá horrivel, arrumando...

---
+++ src/pages/Calculator/index.js
@@ -0,0 +1,128 @@
+import React from 'react';
+import {useState} from 'react';
+import ReactNativeHapticFeedback from 'react-native-haptic-feedback';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
+import {
+    MainCalculator,
+    CalculatorBody,
+    DisplayBody,
+    InputValues,Results,
+    NumericKeyboard,
+    OperatorsKeyboard,
+    Button,
+    ButtonLarge,
+    ButtonClear,
+    Label
+} from './style';
+
+const Calculator = () => {
+        const options = {
+          enableVibrateFallback: true,
+          ignoreAndroidSystemsSettings: false,
+        };
+      
+        ReactNativeHapticFeedback.trigger('impactMedium', options);
+      
+        const [number, setNumber] = useState('');
+        const [result, setResult] = useState();
+      
+        function runCalc() {
+          const resultado = eval(number);
+          setResult(resultado);
+        }
+      
+        function runDelete() {
+          setNumber(number.substring(0, number.length - 1));
+        }
+      
+        function clear() {
+          setNumber('');
+          setResult();
+        }
+      
+        return (
+    
+            <MainCalculator>
+                <CalculatorBody>
+                    {/*Tela da calculadora*/}
+                    <DisplayBody name="tela">
+                        <InputValues
+                            editable={false}
+                            maxLength={10}
+                            keyboardType={'numeric'}
+                            setNumber={text => setNumber(text)}
+                            value={number}
+                        />
+                        <Results editable={false}>
+                            {result}
+                        </Results>
+                    </DisplayBody>
+
+                    {/*Teclado Numérico*/}
+                    <NumericKeyboard>
+                        <Button onPress={() => setNumber(number + '7')}>
+                            <Label>7</Label>
+                        </Button>
+                        <Button onPress={() => setNumber(number + '8')}>
+                            <Label>8</Label>
+                        </Button>
+                        <Button onPress={() => setNumber(number + '9')}>
+                            <Label>9</Label>
+                        </Button>
+                        <Button onPress={() => setNumber(number + '4')}>
+                            <Label>4</Label>
+                        </Button>
+                        <Button onPress={() => setNumber(number + '5')}>
+                            <Label>5</Label>
+                        </Button>
+                        <Button onPress={() => setNumber(number + '6')}>
+                            <Label>6</Label>
+                        </Button>
+                        <Button onPress={() => setNumber(number + '3')}>
+                            <Label>3</Label>
+                        </Button>
+                        <Button onPress={() => setNumber(number + '2')}>
+                            <Label>2</Label>
+                        </Button>
+                        <Button onPress={() => setNumber(number + '1')}>
+                            <Label>1</Label>
+                        </Button>
+                        <ButtonLarge onPress={() => setNumber(number + '0')}>
+                            <Label>0</Label>
+                        </ButtonLarge>
+                        <Button onPress={() => setNumber(number + '.')}>
+                            <Label>.</Label>
+                        </Button>
+                    </NumericKeyboard>
+
+                    {/*Teclado de operações */}
+                    <OperatorsKeyboard>
+                        <Button onPress={() => setNumber(number + '+')}>
+                            <Label> + </Label>
+                        </Button>
+                        <Button onPress={() => setNumber(number + '-')}>
+                            <Label> - </Label>
+                        </Button>
+                        <Button onPress={() => setNumber(number + '*')}>
+                            <Label> * </Label>
+                        </Button>
+                        <Button onPress={() => setNumber(number + '/')}>
+                            <Label> / </Label>
+                        </Button>
+                        <Button onPress={clear}>
+                            <Label> C </Label>
+                        </Button>
+                        <ButtonClear onPress={runDelete}>
+                            <Icon name="backspace" size={21} color="#b71c1c" />
+                        </ButtonClear>
+                        <ButtonLarge onPress={runCalc}>
+                            <Label> = </Label>
+                        </ButtonLarge>
+                    </OperatorsKeyboard>
+                </CalculatorBody>
+            </MainCalculator>
+        );
+};
+
+export default Calculator;
---
+++ src/pages/Calculator/style.js
@@ -0,0 +1,89 @@
+import styled from 'styled-components/native';
+
+export const MainCalculator = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const CalculatorBody = styled.View`
+    flex: 1;
+    background-color: #C5CAE9;
+    flex-direction: row;
+    flex-wrap: wrap;
+    justify-content: space-around;
+    align-content: space-around;
+`;
+
+export const DisplayBody = styled.View`
+    width: 95%;
+    height: 280px;
+    background-color: #A5D6A7;
+    border: solid 8px rgba(159, 168, 218,0.8);
+    justify-content: space-around;
+    align-items: center;
+    border-radius: 4px;
+`;
+
+export const InputValues = styled.TextInput`
+    width: 95%;
+    height: 150px;
+    border-radius: 4px;
+    text-align: right;
+    font-size: 45px;
+`
+
+export const Results = styled.TextInput`
+    width: 95%;
+    height: 150px;
+    border-radius: 4px;
+    text-align: right;
+    font-size: 45px;
+    color: rgba(0,0,0,0.25);
+`
+export const NumericKeyboard = styled.View`
+    width: 200px;
+    height: 42%;
+    flex-direction: row;
+    flex-wrap: wrap;
+    justify-content: space-around;
+    align-content: space-around;
+`;
+
+export const OperatorsKeyboard = styled.View`
+    width: 150px;
+    height: 42%;
+    flex-direction: row;
+    flex-wrap: wrap;
+    justify-content: space-around;
+    align-content: space-around;
+`;
+
+export const Button = styled.TouchableOpacity`
+    width: 60px;
+    height: 60px;
+    background-color: rgba(159, 168, 218,1);
+    border: solid 2px rgba(121, 134, 203,1);
+    justify-content: center;
+    align-items: center;
+    box-shadow: 2px 2px 4px #000;
+    border-radius: 4px;
+`;
+
+export const ButtonLarge = styled(Button)`
+    width: 135px;
+`;
+
+export const ButtonClear = styled(Button)`
+    background-color: #EF5350;
+    border: solid 2px #D32F2F;
+
+`
+
+export const Label = styled.Text`
+    font-size: 21px;
+    font-weight: bold;
+    color: rgba(0,0,0,0.6)
+`;
+
+  
